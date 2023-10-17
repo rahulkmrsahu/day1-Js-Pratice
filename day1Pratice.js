838197c16de96d7678b6158b585ee7174b5782d2
rahulkmrsahu <rahulkmrsahu@gmail.com>
2023-10-17 12:40:30 +0530

    update

--- day1Pratice.js
+++ day1Pratice.js
@@ -1,3 +1,50 @@
-let a = 12
+// To know the Data type of any veriable
+
+let a = 12 // number
 let b = typeof (a)
-console.log(b)
+console.log("Data Type of a =" + b)
+
+
+let name = "Rahul" //String
+
+var dataType = typeof (name)
+console.log("Data Type of name =" + dataType)
+
+
+let isANumber = true //boolean
+var dataType = typeof (isANumber)
+console.log("Data Type of isANumbe =" + dataType)
+
+
+let c = 8888888n //bigint
+var dataType = typeof (c)
+console.log("Data Type of c=" + dataType)
+
+
+let d // undefined
+var dataType = typeof (d)
+console.log("Data Type of d=" + dataType)
+
+let e = null
+var dataType = typeof (e)
+console.log("Data Type of e=" + dataType)
+
+
+let f = Symbol()
+
+var dataType = typeof (f)
+
+console.log("Data Type of f=" + dataType)
+
+
+let g = [1, 2, 3, 4, 5]
+var dataType = typeof (g)
+console.log("Data Type of g=" + dataType)
+
+
+
+
+
+
+
+
---
+++ day2Pratice.js
@@ -0,0 +1,27 @@
+//pop
+
+//let a = [1, 2, 3, 4, 5, 6, 7]
+//let b = a.pop()
+//console.log(a)
+
+//push
+
+//let a = [1, 2, 3, 4, 5, 6, 7]
+//let b = a.push(8, 9, 10)
+//console.log(a)
+
+
+//unshift
+
+//let a = [1, 2, 3, 4, 5, 6, 7]
+//let b = a.(8, 9, 10)
+//console.log(a)
+
+
+//shift
+
+//unshift
+
+let a = [1, 2, 3, 4, 5, 6, 7]
+let b = a.unshift(8)
+console.log(a)
