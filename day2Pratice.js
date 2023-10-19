c111bd522026e3314ea14fb5895b7beacced3fa2
rahulkmrsahu <rahulkmrsahu@gmail.com>
2023-10-19 12:33:37 +0530

    Slice & Splice

--- day2Pratice.js
+++ day2Pratice.js
@@ -4,28 +4,35 @@
 //let b = a.pop()
 //console.log(a)
 
-let boysName = ['Rahul', 'Raju', 'Piyush', 'Prabhat']
-boysName.pop()
-console.log(boysName) // outPut[ 'Rahul', 'Raju', 'Piyush' ]
+// let boysName = ['Rahul', 'Raju', 'Piyush', 'Prabhat']
+// boysName.pop()
+// console.log(boysName) // outPut =[ 'Rahul', 'Raju', 'Piyush' ]
 //push
 
 //let a = [1, 2, 3, 4, 5, 6, 7]
 //let b = a.push(8, 9, 10)
 //console.log(a)
 
+// let city = ['kolkata', 'delhi', 'mumbai', 'pune']
+// let addCity = city.push('bengaleru', 'ambala')
+// console.log(city)// outPut = [ 'kolkata', 'delhi', 'mumbai', 'pune', 'bengaleru', 'ambala' ]  
+
+
 
 //unshift
 
-//let a = [1, 2, 3, 4, 5, 6, 7]
-//let b = a.(8, 9, 10)
-//console.log(a)
+// let number = [1, 2, 3, 4, 5, 6, 7]
+// let unshiftNumber = number.unshift(8, 9, 10)
+// console.log(number)
 
 
 //shift
 
-//unshift
 
-let a = [1, 2, 3, 4, 5, 6, 7]
-let b = a.unshift(8)
-console.log(a)
 
+// let a = [1, 2, 3, 4, 5, 6, 7]
+// let b = a.shift()
+// console.log(a)
+
+
+let myName = ['rahul', 'raju', 'dinesh', 'suresh', 'mukesh']
---
+++ day3Pratice.js
@@ -0,0 +1,20 @@
+//Slice
+
+let a = [1, 2, 3, 4, 5, 6]
+
+
+let b = a.slice(4, 6)
+
+
+console.log(b)
+
+
+// //splice
+
+
+let fruits = ['apple', ',mango', 'banana', 'kiwi']
+
+
+let addFruits = fruits.splice(2, 2, 'waterMelon', 'gwava')
+
+console.log(fruits)
