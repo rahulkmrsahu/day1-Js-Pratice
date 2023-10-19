f6dc55ebc2fb29c66a57653b80a419388bf2d31c
rahulkmrsahu <rahulkmrsahu@gmail.com>
2023-10-19 10:57:57 +0530

    update

--- day2Pratice.js
+++ day2Pratice.js
@@ -4,6 +4,9 @@
 //let b = a.pop()
 //console.log(a)
 
+let boysName = ['Rahul', 'Raju', 'Piyush', 'Prabhat']
+boysName.pop()
+console.log(boysName) // outPut[ 'Rahul', 'Raju', 'Piyush' ]
 //push
 
 //let a = [1, 2, 3, 4, 5, 6, 7]
@@ -25,3 +28,4 @@
 let a = [1, 2, 3, 4, 5, 6, 7]
 let b = a.unshift(8)
 console.log(a)
+
