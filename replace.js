04d162bb2eeb059745aa80f3db6c921bb8bf2ba4
rahulkmrsahu <rahulkmrsahu@gmail.com>
2023-10-27 12:18:11 +0530

    update

--- replace.js
+++ replace.js
@@ -1,9 +1,9 @@
 
 
 
-let fName = "Rahul"
-let newName = fName.replace("Rahul", "raju")
-console.log(newName)
+// let fName = "Rahul"
+// let newName = fName.replace("Rahul", "raju")
+// console.log(newName)
 
 
 // concatination..
@@ -33,5 +33,23 @@ console.log(newName)
 
 // console.log(newStr.length)
 
+// Split
 
+// let str = "I love you   javaScript"
+// let newStr = str.split("  ")
+// console.log(newStr)
+
+
+// const items = "apple, banana , orange , mango"
+// const fruits = items.split(",")
+// console.log(fruits)
+
+
+// let text = "Hello everyone"
+// const arr = text.split(" ", 2)
+// console.log(arr)
+
+// const date = "2023-08-04"
+// const parts = date.split("-")
+// console.log(parts)
 
---
+++ slice.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4]
+
+let newArr = arr.slice(1, 1)
+
+console.log(arr)
