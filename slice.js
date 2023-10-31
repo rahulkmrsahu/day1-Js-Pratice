0632911bd043717c6c4b7a55b64bddda1e1450c8
rahulkmrsahu <rahulkmrsahu@gmail.com>
2023-10-31 10:59:24 +0530

    update

--- slice.js
+++ slice.js
@@ -1,5 +1,6 @@
-let arr = [1, 2, 3, 4]
+let arr = [1, 2, 3, 4, 5]
 
-let newArr = arr.slice(1, 1)
+let newArr = arr.slice(2, 4)
 
+console.log(newArr)
 console.log(arr)
---
+++ splice.js
@@ -0,0 +1,13 @@
+// let a = [1, 2, 3, 4, 5]
+// let e = a.splice(3, 2)
+// console.log(a)
+
+// let a = [1, 2, 3, 4, 5]
+// let e = a.splice(2, 3, "hh")
+// console.log(a)
+// console.log(e)
+
+let a = [1, 2, 3, 4, 5]
+let e = a.splice(1)
+console.log(a)
+console.log(e)
